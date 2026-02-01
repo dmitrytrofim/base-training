82f976a1112b2139a9736208b41402a7f62e9fd4
dmitrytrofim <d.trofimov@jetmail.cc>
2026-02-01 08:56:11 +0300

    add possibility training js/ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/index.css'
 import '@/assets/scss/index.scss'
+import '@/assets/javascript/index.js'
+import '@/assets/typescript/index.ts'
 
 createApp(App).mount('#app')
