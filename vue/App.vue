2a8fe2b620991a71f0d2cd75fa9f0f58059b81a6
dmitrytrofim <d.trofimov@jetmail.cc>
2026-02-22 08:53:03 +0300

    remove prettier

--- vue/App.vue
+++ vue/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="p-[5px]">
-    <div class="flex items-center gap-[10px] text-[16px]">
+    <div class="flex items-center gap-[10px] text-[16px] border-b pb-[10px]">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/playground/composition-api">CompositionApi</RouterLink>
       <RouterLink to="/playground/options-api">OptionsApi</RouterLink>
     </div>
-    <div class="">
+    <div class="pt-[10px]">
       <RouterView />
     </div>
   </div>
